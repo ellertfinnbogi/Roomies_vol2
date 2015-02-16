f7da6655e8a05b5aeb9b610aa78d84fa4cd4997a
Ellert Finnbogi <ellertfinnbogi@gmail.com>
2015-02-16 16:38:06 +0000

    Log-in virkar og hægt að skrá nýjan user

---
+++ views/logged_in.php
@@ -0,0 +1,30 @@
+<?php 
+require_once("../classes/Login.php");
+// include the configs / constants for the database connection
+require_once("../config/db.php");
+
+// load the login class
+
+
+
+$login = new Login();
+
+// ... ask if we are logged in here:
+if ($login->isUserLoggedIn() == true) {
+    // the user is logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are logged in" view.
+   // include("views/logged_in.php");
+    //header("Location:views/logged_in.php");
+    echo 'Everything works now!';
+
+} else {
+    // the user is not logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are not logged in" view.
+    include("header.html");
+}
+
+//Hey, <?php echo $_SESSION['user_name']; ?>
+
+<!-- if you need user information, just put them into the $_SESSION variable and output them here -->
+<!-- because people were asking: "index.php?logout" is just my simplified form of "index.php?logout=true" -->
+<a href="../index.php?logout">Logout</a>

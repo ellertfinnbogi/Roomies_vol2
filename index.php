aef1ae8176df13be7f1add1e5693c494d3bb3bf2
ats15 <ats15@hi.is>
2015-02-17 21:50:31 +0000

    Committ
    Utskyring

--- index.php
+++ index.php
@@ -23,14 +23,14 @@ if (version_compare(PHP_VERSION, '5.3.3', '<')) {
 // include the configs / constants for the database connection
 require_once("config/db.php");
 
-// load the login class
+// Hlada load i login class
 require_once("classes/Login.php");
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process. in consequence, you can simply ...
 $login = new Login();
 
-// ... ask if we are logged in here:
+// ... athuga hvort tu sert loggadur inn her:
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.

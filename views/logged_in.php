4f536bbccc90fbc353d5947ce7c39fd8c39db7d9
Ellert Finnbogi <ellertfinnbogi@gmail.com>
2015-03-02 15:32:08 +0000

    update

--- views/logged_in.php
+++ views/logged_in.php
@@ -1,6 +1,20 @@
 
 
 Hey, <?php echo $_SESSION['user_name']; 
+require_once("../classes/Login.php");
+
+$temp;
+$temp->isUserLoggedIn();
+
+if($temp)
+{
+	echo 'successs!!';
+
+}
+else
+{
+	echo 'failed';
+}
 
 
 ?>. You are logged in.

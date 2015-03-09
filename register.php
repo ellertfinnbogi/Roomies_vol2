02d15547140313e172cca26c40a0d87e2ace5f64
Ellert Finnbogi <ellertfinnbogi@gmail.com>
2015-03-09 22:36:17 +0000

    loka push fyrir sprett2

--- register.php
+++ register.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * PHP login script / Vid reynum ad nota audveldan hreinan koda.
- * Getum notad www.php-login.net til frekari adstodar vid log in.
- *
- * Notkun a PHP Session og password-hashing kemur fram her sem er naudsynlegt fyrir kerfid.
- *
- * @author Panique hofundur
- * @link https://github.com/panique/php-login-minimal/ github kennsla
- * @license http://opensource.org/licenses/MIT MIT License - leyfid 
- */
 
 // Skodum her lagmarks PHP utgafu
 
@@ -21,8 +11,9 @@
     // Ef vid stydjumst vid PHP 5.3 eda 5.4 verdum vid ad includa password_api_compatibility_library.php
     // Tilgangur tessa safns er ad baeta PHP5.5 password hashing fyrir function i eldri utgafum af PHP
     //ekki í notkun eins og er.
-    require_once("libraries/password_compatibility_library.php");
+require_once("libraries/password_compatibility_library.php");
 }
+//require_once("views/header.html");
 
 // include configid fyrir breyturnar i tengingunni vid database
 require_once("config/db.php");
@@ -35,4 +26,46 @@ require_once("classes/Registration.php");
 $registration = new Registration();
 
 // synir register view, med registration form, skilabod og villuskilabod
-include("views/register.php");
+//include("views/register.php");
+
+//Mogulegar villur
+if (isset($registration)) {
+    if ($registration->errors) {
+        foreach ($registration->errors as $error) {
+            echo $error;
+        }
+    }
+    if ($registration->messages) {
+        foreach ($registration->messages as $message) {
+            echo $message;
+        }
+    }
+}
+?>
+
+     <meta charset="utf-8" >
+     <link rel="stylesheet" href="css/look.css">
+     <link rel='shortcut icon' type='image/x-icon' href='img/roomies.ico' />
+<form method="post" action="register.php" name="registerform">
+
+    
+    <label for="login_input_username">Notendanafn (aðeins stafir og tölusaftir, 2 til 64 slög)</label>
+    <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
+    <br />
+
+    
+    <label for="login_input_email">Netfang notenda</label>
+    <input id="login_input_email" class="login_input" type="email" name="user_email" required />
+    <br />
+    <label for="login_input_password_new">Lykilorð (lágmark 6 stafir)</label>
+    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
+    <br />
+    <label for="login_input_password_repeat">Endurtaka lykilorð</label>
+    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
+    <br />
+    <input type="submit"  name="register" value="Búa til aðgang" />
+
+</form>
+
+
+<a href="index.php">Tilbaka á innskráningarsíðu</a>
--- views/logged_in.php
+++ views/logged_in.php
@@ -18,7 +18,7 @@ if($_SESSION['user_login_status'] == 1 && $res['room'] != null)
 {
 
 	
-	echo "<h2>VERKEFNASKRÁ</h2>";
+	echo "<h2>VERKEFNASKRÁ fyrir herbergi: ".$_SESSION['room']." </h2>" ;
 	?>
 	<!-- ***Hérna er formið fyrir að skrá inní todo  *** -->
 	<script src="../js/display_functions.js"></script>
@@ -71,17 +71,16 @@ if($_SESSION['user_login_status'] == 1 && $res['room'] != null)
 
 
 	$sql = "SELECT user_name,todo,user_resp,do_date from todo where room = '" . $_SESSION['room'] ."';";
-	//$sql = "SELECT todo FROM todo where user_name =". $_SESSION['user_name'] . ";";
 	
 	$result = $conn->query($sql);
 	
 
-	echo 	"<table><tr><th>Hver skáir</th><th>Todo</th><th>Hver á verk</th><th>Klára fyrir</th></tr>";
+	echo 	"<table><tr><th>Hver skáir</th><th id='todo'>Todo</th><th>Hver á verk</th><th>Klára fyrir</th><th>Verk klárað?</th></tr>";
 	if($result->num_rows > 0)
 	{
 		while($row = $result->fetch_assoc())
 		{
-			echo "<tr><td>". $row['user_name'] ."</td><td>". $row['todo'] . "</td><td>". $row['user_resp']. "</td><td>". $row['do_date']. "</td></tr>";
+			echo "<tr><td>". $row['user_name'] ."</td><td>". $row['todo'] . "</td><td>". $row['user_resp']. "</td><td>". $row['do_date']. "</td> <td><input type='checkbox'></td></tr>";
 			
 		}
 
@@ -89,57 +88,7 @@ if($_SESSION['user_login_status'] == 1 && $res['room'] != null)
 	}
 	else{echo "</table>";}
 
-	?>
-	<!-- ***Hérna er formið fyrir að skrá inní todo  *** -->
-	<script src="../js/display_functions.js"></script>
-	<div class="makejob">
-	   <a href="#" onclick="toggle_visibility('list1');">
-      <p>Skrá verk</p>
-      </a>
-	<div id="list1" class="container">
-		
-	         <form method="post" action="<?php $_SERVER['SCRIPT_NAME']?>" name="savejob">
-	         <ul>
-	         <li>
-	            <label for="todo">Todo </label>
-	             <input id="todo_list" type="text" name="todo_list" required>
-	             </li>
-	             <li>
-	             
-	             <label for="personresponsible">Hver á að gera verk </label>
-	             <?php
-	             echo "<select name='personresponsible'>";
-					echo '<option value="">'.'--- Veldu hérna ---'.'</option>';
-					$query = "SELECT user_name FROM users where room = '". $_SESSION['room'] ."';";
-					
-					$results = $conn->query($query);
-
-					if($results->num_rows > 0)
-					{
-						while($row = $results->fetch_assoc())
-						{
-							echo $row['user_name'];
-							echo "<option>". $row['user_name']
-							 .'</option>';	
-						}
-					}
-					echo '</select>';
-
-	             ?>
-	             </li>
-	             <li>
-	             <label for="date">Dagsetning</label>
-	             <input id="date" type="date" name="date" required />
-	             </li>
-	             
-	             <input type="submit"  name="savejob" value="Skrá verk" />
-	             </ul>
-	         </form>
-        </div>
-	</div>
-
-	<?php
-	
+		//sér um að skrá í gagnagrunn ef klikkað er á Skrá verk
 		$user_function = new UserFunctions();
         if(isset($_POST["savejob"])) {
             $user_function->savejob();
@@ -233,7 +182,8 @@ echo $form;
 }
 else 
 {
-	die('Run you fool!');
+	die('Þú hefur ekki aðgang að þessari síðu');
+	printf("<script>location.href='../index.php'</script>");
 }
 ?>
 
